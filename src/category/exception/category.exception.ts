1401d7846338acb7e8c0a35274c6c3b16cdada0e
JangAJang <janghee5395@gmail.com>
2024-01-11 18:12:57 +0900

    feat: 예외 클래스에 정적 메서드로 swagger 문서 로직을 반환하는 기능 구현

--- src/category/exception/category.exception.ts
+++ src/category/exception/category.exception.ts
@@ -1,8 +1,14 @@
+import { createApiResponseOption } from 'src/util/swagger.util';
 import { HttpNotFoundException } from '../../util/exception.util';
+import { NOT_FOUND } from 'src/constant/constant';
 
 class CategoryNotFoundException extends HttpNotFoundException {
   constructor() {
     super('카테고리가 존재하지 않습니다.', 'C02');
+  }
+
+  static response() {
+    return createApiResponseOption(NOT_FOUND, 'C02', null);
   }
 }
 
