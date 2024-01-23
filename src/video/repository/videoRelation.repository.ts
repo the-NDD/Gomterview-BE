39fbfe4811f0aeba440d634594b589c3c30eebd0
JangAJang <janghee5395@gmail.com>
2024-01-23 10:59:46 +0900

    feat: repository에서 VideoRelation을 통해서 영상의 id로 연관영상들을 불러오는 로직 구현

---
+++ src/video/repository/videoRelation.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { VideoRelation } from '../entity/videoRelation';
+
+@Injectable()
+export class VideoRelationRepository {
+  constructor(
+    @InjectRepository(VideoRelation)
+    private repository: Repository<VideoRelation>,
+  ) {}
+
+  async insert(videoRelation: VideoRelation) {
+    await this.repository.insert(videoRelation);
+  }
+
+  async delete(videoRelation: VideoRelation) {
+    await this.repository.delete({ id: videoRelation.id });
+  }
+
+  async findChildrenByParentId(parentId: number) {
+    const relations = await this.repository
+      .createQueryBuilder('VideoRelation')
+      .leftJoinAndSelect('VideoRelation.parent', 'parent')
+      .leftJoinAndSelect('VideoRelation.child', 'child')
+      .where('parent.id = :parentId', { parentId })
+      .getMany();
+    return relations.map((relation) => relation.child);
+  }
+}
