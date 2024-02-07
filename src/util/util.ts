e8d58406f44404ba75c5082ab5b9d23280a8f67e
JangAJang <janghee5395@gmail.com>
2024-02-07 15:17:41 +0900

    refactor: MemberVideoResponse의 createdAt을 number -> yyyy.MM.DD로 반환하게 수정

---
+++ src/util/util.ts
@@ -0,0 +1,4 @@
+export const parseDateToString = (date: Date) =>
+  `${date.getFullYear()}.${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`;
